ba87c1455d1d541461a5bee0e3081639e833c1dd
Sushant Paudel <sushantpaudel77@gmail.com>
2025-08-05 20:56:22 +0545

    ADD: slf4j logs in controller

--- serverside/src/main/java/com/websocket_realtime_chatapp/websocket_chatapp/controller/ChatController.java
+++ serverside/src/main/java/com/websocket_realtime_chatapp/websocket_chatapp/controller/ChatController.java
@@ -1,18 +1,23 @@
 package com.websocket_realtime_chatapp.websocket_chatapp.controller;
 
 import com.websocket_realtime_chatapp.websocket_chatapp.model.ChatMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+import java.util.Objects;
+
+@Slf4j
 @Controller
 public class ChatController {
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        log.info("Received message from {}: {}", chatMessage.getSender(), chatMessage.getContent());
         return chatMessage;
     }
 
@@ -20,7 +25,9 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage,
                                SimpMessageHeaderAccessor headerAccessor) {
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        String username = chatMessage.getSender();
+        Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", chatMessage.getSender());
+        log.info("New user added: {}", username);
         return chatMessage;
     }
 }
