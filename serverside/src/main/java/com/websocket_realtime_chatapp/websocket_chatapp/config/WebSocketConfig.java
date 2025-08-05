e6fe635d97dad45f0d953d5b9af0c836a166bee4
Sushant Paudel <sushantpaudel77@gmail.com>
2025-08-05 21:05:32 +0545

    ADD: slf4j logs in config

--- serverside/src/main/java/com/websocket_realtime_chatapp/websocket_chatapp/config/WebSocketConfig.java
+++ serverside/src/main/java/com/websocket_realtime_chatapp/websocket_chatapp/config/WebSocketConfig.java
@@ -1,11 +1,15 @@
 package com.websocket_realtime_chatapp.websocket_chatapp.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -14,6 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
+        log.info("Message broker configured with simple broker '/topic' and prefix '/app'");
     }
 
     @Override
@@ -21,5 +26,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
+        log.info("WebSocket endpoint '/ws' registered with SockJS and CORS allowed");
     }
 }
